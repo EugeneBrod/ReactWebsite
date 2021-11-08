ab2e6457dc15f6a93eb882bff1f45da06191787d
EugeneBrod <eugenebrod@gmail.com>
2021-11-08 14:50:53 -0800

    replaced old socials table with react (planets) socials

--- react_website/src/App.js
+++ react_website/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Planet } from 'react-planet';
 
 function App() {
   return (
@@ -8,54 +9,135 @@ function App() {
   <h1>Hi. I'm Eugene.</h1>
   <div class="objective">
     <p>
-      <span>Welcome, make yourself <a href="https://www.youtube.com/watch?v=PMgN6-CUoLQ&ab_channel=EllusionStudio">comfortable.</a></span>
+      <span><a href="https://www.youtube.com/watch?v=PMgN6-CUoLQ&ab_channel=EllusionStudio">Welcome.</a></span>
     </p>
     <p>
     <span>I studied math and cs at <a href="https://www.berkeley.edu/">UC Berkeley.</a></span>
     </p>
     <p>
-    <span><a href="https://en.wikipedia.org/wiki/Russia">Russian</a> born, <a href="https://en.wikipedia.org/wiki/San_Francisco_Bay_Area">Bay Area</a> native.</span>
-    </p>
-    <p>
       <span>Here is my <a download href='eugene_brodsky_resume.pdf'>resume.</a></span>
     </p>
   </div>
-  <table class="social table">
-    <tr>
-      <td>
-        <a href="https://github.com/EugeneBrod">
-          <div>
-            <i class="fab fa-github"></i>
+
+  <div class="planets">
+    <Planet
+        orbitStyle={defaultStyle => ({
+          ...defaultStyle,
+          borderWidth: 0,
+        })}
+        centerContent={
+          <div
+            style={{
+              height: 100,
+              width: 100,
+              borderRadius: '50%',
+              backgroundColor: '#c7fffd',
+              position: 'absolute', left: '50%', top: '50%',
+              transform: 'translate(-50%, -50%)'
+            }}
+          >
+            <div style={{
+              position: 'absolute', left: '50%', top: '50%',
+              transform: 'translate(-50%, -50%)'
+            }}
+            >
+              <b>Links</b>
+            </div>
           </div>
-          <p>Github</p>
-        </a>
-      </td>
-      <td>
-        <a href="https://www.linkedin.com/in/eugene-brodsky-a78759148/">
-          <div>
-            <i class="fab fa-linkedin-in"></i>
-          </div>
-          <p>LinkedIn</p>
-        </a>
-      </td>
-      <td>
-        <a href="https://www.instagram.com/eugenebrod.sky/">
-          <div>
-            <i class="fab fa-instagram"></i>
-          </div>
-          <p>Instagram</p>
-        </a>
-      </td>
-      <td>
-        <a href="mailto:eugenebrod@eugenebrodsky.me">
-          <div>
-            <i class="fas fa-envelope"></i>
-          </div>
-          <p>Email</p>
-        </a>
-      </td>
-    </tr>
-  </table>
+        }
+        dragablePlanet
+        open
+        autoClose
+        orbitRadius={150}
+        rotation={90}
+        
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div
+          style={{
+            height: 70,
+            width: 70,
+            borderRadius: '50%',
+            backgroundColor: '#eabdff',
+          }}
+        >
+          <div style={{
+              position: 'absolute', left: '50%', top: '50%',
+              transform: 'translate(-50%, -50%)'
+            }}
+            >
+              <a href="https://github.com/EugeneBrod">
+                <div>
+                  <i class="fab fa-github"></i>
+                </div>
+              </a>
+            </div>
+        </div>
+        <div
+          style={{
+            height: 70,
+            width: 70,
+            borderRadius: '50%',
+            backgroundColor: '#eabdff',
+          }}
+        >
+          <div style={{
+              position: 'absolute', left: '50%', top: '50%',
+              transform: 'translate(-50%, -50%)'
+            }}
+            >
+              <a href="https://www.linkedin.com/in/eugene-brodsky-a78759148/">
+                <div>
+                  <i class="fab fa-linkedin-in"></i>
+                </div>
+              </a>
+            </div>
+        </div>
+        <div
+          style={{
+            height: 70,
+            width: 70,
+            borderRadius: '50%',
+            backgroundColor: '#eabdff',
+          }}
+        >
+          <div style={{
+              position: 'absolute', left: '50%', top: '50%',
+              transform: 'translate(-50%, -50%)'
+            }}
+            >
+              <a href="https://www.instagram.com/eugenebrod.sky/">
+                <div>
+                  <i class="fab fa-instagram"></i>
+                </div>
+              </a>
+            </div>
+        </div>
+        <div
+          style={{
+            height: 70,
+            width: 70,
+            borderRadius: '50%',
+            backgroundColor: '#eabdff',
+          }}
+        >
+          <div style={{
+              position: 'absolute', left: '50%', top: '50%',
+              transform: 'translate(-50%, -50%)'
+            }}
+            >
+              <a href="mailto:eugenebrod@eugenebrodsky.me">
+                <div>
+                  <i class="fas fa-envelope"></i>
+                </div>
+              </a>
+            </div>
+        </div>
+      </Planet>
+  </div>
 </body>
   );
 }
