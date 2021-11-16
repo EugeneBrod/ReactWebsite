72511de6ba63c62922a32716731a4132c912afab
EugeneBrod <eugenebrod@gmail.com>
2021-11-15 20:38:56 -0800

    spelling fix

--- react_website/src/components/CraigslistScraper.js
+++ react_website/src/components/CraigslistScraper.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 function CraigslistScraper() {
   return (
     <body>
-      <p>Under Construction. Feature comming soon!</p>
+      <p>Under Construction. Features coming soon!</p>
     </body>
   )
 };
