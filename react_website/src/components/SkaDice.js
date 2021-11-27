3ad1f26adc664b1c3ce7ecce1bf417697f2a5ad1
EugeneBrod <eugenebrod@gmail.com>
2021-11-26 16:08:07 -0800

    commiting to purge large files

--- react_website/src/components/SkaDice.js
+++ react_website/src/components/SkaDice.js
@@ -12,8 +12,11 @@ export class SkaDice extends Component {
         <p>This was the first App that I created. Mainly focusing on the basics of creating iOS layouts and working with view controllers.</p>
         <p>I found the existing skate dice apps to lack a variety of tricks because they actually implmented dice logic.</p>
         <p>SkaDice offers a wider selection of tricks so that skaters can step out of their comfort zone and try manuvers that they may not normally try.</p>
+        <h1>Interactive home screen.</h1>
         <img src={ pic_1 } alt="pic_1"/>
         <img src={ pic_2 } alt="pic_2"/>
+        <br></br>
+        <h1>Large selection of tricks.</h1>
         <img src={ pic_3 } alt="pic_3"/>
       </div>
     );
