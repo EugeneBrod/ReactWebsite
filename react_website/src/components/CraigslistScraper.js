f537a4e39991de4b8db1af3c961898b92e5aaf46
EugeneBrod <eugenebrod@gmail.com>
2021-11-27 23:24:20 -0800

    Remove Stop button

--- react_website/src/components/CraigslistScraper.js
+++ react_website/src/components/CraigslistScraper.js
@@ -30,37 +30,47 @@ export class CraigslistScraper extends Component {
   }
 
   startButtonHandler(event) {
-    const path = BASEURL + '/setSettings';
-    const payload = {
-      recipient_emails: this.state.recipientList.split(/[ ,]+/),
-      urls: this.state.targetURLs.split(/[ ,]+/),
-    };
-    console.log(payload)
-    axios.post(path, payload)
+    const path = BASEURL + '/stop';
+    this.occupied = false;
+    axios.get(path)
       .then((res) => {
-        if (res.data.success) {
-          const path = BASEURL + '/start';
-          axios.get(path)
+          const path = BASEURL + '/setSettings';
+          const payload = {
+            recipient_emails: this.state.recipientList.split(/[ ,]+/),
+            urls: this.state.targetURLs.split(/[ ,]+/),
+          };
+          console.log(payload)
+          axios.post(path, payload)
             .then((res) => {
               if (res.data.success) {
-                this.setState({msg: "Launched Scraper"})
-              } else {
-                this.setState({msg: "Scraper already in use."})
+                const path = BASEURL + '/start';
+                axios.get(path)
+                  .then((res) => {
+                    if (res.data.success) {
+                      this.setState({msg: "Launched Scraper"})
+                    } else {
+                      this.setState({msg: "Scraper already in use."})
 
+                    }
+                  })
+                  .catch((error) => {
+                    this.setState({msg: "Something went wrong."})
+                  });
+              } else {
+                this.setState({msg: "Something went wrong"})
               }
+              
             })
             .catch((error) => {
+              console.log(error);
               this.setState({msg: "Something went wrong."})
             });
-        } else {
-          this.setState({msg: "Something went wrong"})
-        }
-        
-      })
+          })
       .catch((error) => {
-        console.log(error);
         this.setState({msg: "Something went wrong."})
       });
+
+    
   }
 
   stopButtonHandler(event) {
@@ -87,16 +97,13 @@ export class CraigslistScraper extends Component {
         <p>This is an interface to the Craigslist Scraper that I have built.</p>
         <p>It's been dockerized to run on Google's Cloud Run.</p>
         <br/>
-        <p>If you do not recieve an email right away, try pressing the stop button. This will kick out the current user of the scraper.</p>
-        <p>Survival of the most persistent I suppose.</p>
-        <br/>
         <p>EXAMPLE:</p>
         <p>left box: https://sfbay.craigslist.org/d/for-sale/search/sss, https://sfbay.craigslist.org/search/ata?purveyor-input=all&max_price=2000</p>
         <p>right box: some_email@gmail.com, another_email@gmail.com</p>
         <input className="targetURLs" type="text" onChange={(e) => this.targetURLsHandler(e)} placeholder="URLs to scrape."></input>
         <input className="recipientList" type="text" onChange={(e) => this.recipientListHandler(e)} placeholder="recipients to notify of new posts"></input>
         <button className="startButton" onClick={this.startButtonHandler}>Start</button>
-        <button className="stopButton" onClick={this.stopButtonHandler}>Stop</button>
+        {/* <button className="stopButton" onClick={this.stopButtonHandler}>Stop</button> */}
         <p className="ErrorMsg">{ this.state.msg }</p>
       </div>
     );
