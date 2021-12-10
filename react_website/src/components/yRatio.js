0c247e09ad4050e422de580354e198702c7c1a72
Eugene Brodsky <diesel@Eugenes-MBP.attlocal.net>
2021-12-10 02:20:50 -0800

    "Added YoutubeRatio image."

--- react_website/src/components/yRatio.js
+++ react_website/src/components/yRatio.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import yRatio from './../assets/yRatio.jpeg'
 
 export class YRatio extends Component {
 
@@ -6,6 +7,7 @@ export class YRatio extends Component {
     return (
       <div className="yRatio">
         <p>Install YoutubeRatio <a target='_blank' href='https://chrome.google.com/webstore/detail/yratio/cdeikckdpancilnmeojckofeoojhdkal'>here.</a></p>
+        <img src={ yRatio } alt='yRatio'/>
       </div>
     );
   }
