b76b711ee41577d0b090c33b03020d4713dec0b2
Eugene Brodsky <eugene@gomotion.ai>
2023-04-24 20:43:35 -0700

    Edits nav bar to remove unwanted pages

--- react_website/src/components/Navbar.js
+++ react_website/src/components/Navbar.js
@@ -10,9 +10,6 @@ function Navbar() {
             <td><Link to='/'>About</Link></td>
             <td><Link to='/cScraper'>cScraper</Link></td>
             <td><Link to='/YoutubeRatio'>YoutubeRatio</Link></td>
-            <td><Link to='/Horse'>Horse</Link></td>
-            <td><Link to='/SkaDice'>SkaDice</Link></td>
-            <td><Link to='/TestPage'>TestPage</Link></td>
           </tr>
         </tbody>
       </table>
